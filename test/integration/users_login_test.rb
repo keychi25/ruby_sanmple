6d12b6845851980081bcbfc9d06206bcc20e35f0
keychi25 <e165412y@st.u-gakugei.ac.jp>
2018-10-23 13:08:39 +0900

    Finish user edit, update, index, and destroy actions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-
   def setup
     @user = users(:michael)
   end
@@ -16,18 +15,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test "login with valid information" do
-    get login_path
-    post login_path, params: { session: { email:    @user.email,
-                                          password: 'password' } }
-    assert_redirected_to @user
-    follow_redirect!
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
-    assert_select "a[href=?]", user_path(@user)
-  end
-
   test "login with valid information followed by logout" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
@@ -39,6 +26,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
@@ -48,11 +36,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+
   end
 
   test "login with remembering" do
-      log_in_as(@user, remember_me: '1')
-      assert_equal cookies['remember_token'], assigns(:user).remember_token
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
   end
 
   test "login without remembering" do
